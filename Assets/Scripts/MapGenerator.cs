45118a644dbe1bb0c500bacdebb819fb8c60265f
Jerry123i <igorsc.jerry@gmail.com>
2019-09-10 16:07:09 -0300

    Bigger Walls

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -42,132 +42,50 @@ public class MapGenerator : MonoBehaviour
             SpawnTile(slotFloor, position);
 
         if (pixelColor == Color.black)
+            SpawnTile(emptyWall, position);
+
+        if (pixelColor == Color.blue)
         {
-            SpawnTile(emptyFloor, position);
 
             Color up = map.GetPixel(x, y + 1);
             Color down = map.GetPixel(x, y - 1);
             Color right = map.GetPixel(x + 1, y);
             Color left = map.GetPixel(x - 1, y);
 
-            if(isEmptySlot(up) && isEmptySlot(down))
-            {
-                if (up == Color.white || up == Color.red)
-                    SpawnTile(emptyWall, position, Quaternion.Euler(0, 0, 180f));
-                if (down == Color.white || down == Color.red)
-                    SpawnTile(emptyWall, position);
-            }
-            else if (isEmptySlot(right) && isEmptySlot(left))
-            {
-                if (right == Color.white || right == Color.red)
-                    SpawnTile(emptyWall, position, Quaternion.Euler(0, 0, 90f));
-                if (left == Color.white || left == Color.red)
-                    SpawnTile(emptyWall, position, Quaternion.Euler(0, 0, -90f));
-            }
-
-            else if(isFloorSlot(left) && right == Color.black 
-                && ((isFloorSlot(up) && down == Color.black) || (isFloorSlot(down) && up == Color.black)) 
-                && (map.GetPixel(x+1,y-1) == Color.black || map.GetPixel(x+1,y+1) == Color.black))
-            {
-                SpawnWallFacingLeft(position);
-            }
-
-            else if(isFloorSlot(up) && down == Color.black 
-                && ((isFloorSlot(right) && left == Color.black) || (isFloorSlot(left) && right == Color.black))
-                &&(map.GetPixel(x - 1, y - 1) == Color.black || map.GetPixel(x + 1, y - 1) == Color.black))
-            {
+            if (up == Color.white && down == Color.black)
                 SpawnWallFacingUp(position);
-            }
-
-            else if (isFloorSlot(right) && left == Color.black
-                && ((isFloorSlot(down) && up == Color.black)||(isFloorSlot(up) && down == Color.black))
-                && (map.GetPixel(x - 1, y + 1) == Color.black || map.GetPixel(x - 1, y - 1) == Color.black))
-            {
+            else if (right == Color.white && left == Color.black)
                 SpawnWallFacingRight(position);
-            }
-
-            else if((isFloorSlot(down) && up == Color.black
-                && ((isFloorSlot(left) &&  right == Color.black )||(isFloorSlot(right) && left == Color.black)))
-                && (map.GetPixel(x - 1, y + 1) == Color.black || map.GetPixel(x + 1, y + 1) == Color.black))
-            {
-                SpawnWallFacingDown(position);
-            }
-
-            else if((up == Color.white || up == Color.red) && down == Color.black)
-            {
-                SpawnTile(emptyWall, position, Quaternion.Euler(0, 0, 180f));
-            }
-
-            else if((down == Color.white || down == Color.red) && up == Color.black)
-            {
-                SpawnTile(emptyWall, position);
-            }
-
-            else if((right == Color.white || right == Color.red) && down == Color.black)
-            {
-                SpawnTile(emptyWall, position, Quaternion.Euler(0, 0, 90f));
-            }
-
-            else if ((left== Color.white || left == Color.red)&& right == Color.black)
-            {
-                SpawnTile(emptyWall, position, Quaternion.Euler(0, 0, -90f));
-            }
-
-
-
-
-            //else if(right == Color.black && down == Color.black && (map.GetPixel(x+1,y-1) == Color.white))
-            //{
-            //    SpawnTile(emptyWall, position, Quaternion.Euler(0, 0, -90f));
-            //    SpawnTile(emptyWall, position);
-            //}
-
-            //else if(down == Color.black && left == Color.black && (map.GetPixel(x-1,y-1) == Color.white))
-            //{
-            //    SpawnTile(emptyWall, position);
-            //    SpawnTile(emptyWall, position, Quaternion.Euler(0, 0, 90f));
-            //}
-
-            //else if(left == Color.black && up == Color.black && (map.GetPixel(x-1,y+1) == Color.white))
-            //{
-            //    SpawnTile(emptyWall, position, Quaternion.Euler(0, 0, 90f));
-            //    SpawnTile(emptyWall, position, Quaternion.Euler(0,0,180f));
-            //}
-
-            //else if(up == Color.black && right == Color.black && (map.GetPixel(x+1,y+1) == Color.white))
-            //{
-            //    SpawnTile(emptyWall, position, Quaternion.Euler(0, 0, 180f));
-            //    SpawnTile(emptyWall, position, Quaternion.Euler(0, 0, -90f));
-            //}
-
-
+            else if (down == Color.white && up == Color.black)
+                SpawnWallFacingRight(position);
+            else if (left == Color.white && right == Color.black)
+                SpawnWallFacingLeft(position);
+            else
+                SpawnTile(emptyFloor, position);
 
         }
-
-        if (pixelColor == Color.blue)
-            SpawnTile(slotWall, position);
 
 
     }
 
     void SpawnWallFacingLeft(Vector2 position)
     {
-        SpawnTile(emptyWall, position, Quaternion.Euler(0, 0, -90f));
+        SpawnTile(slotWall, position, Quaternion.Euler(0, 0, -90f));
     }
 
     void SpawnWallFacingUp(Vector2 position)
     {
-        SpawnTile(emptyWall, position, Quaternion.Euler(0, 0, 180f));
+        SpawnTile(slotWall, position, Quaternion.Euler(0, 0, 180f));
     }
 
     void SpawnWallFacingRight(Vector2 position)
     {
-        SpawnTile(emptyWall, position, Quaternion.Euler(0, 0, 90f));
+        SpawnTile(slotWall, position, Quaternion.Euler(0, 0, 90f));
     }
 
     void SpawnWallFacingDown(Vector2 position)
     {
-        SpawnTile(emptyWall, position);
+        SpawnTile(slotWall, position);
     }
 
     void SpawnTile(GameObject prefab, Vector2 position, Quaternion rotation)
